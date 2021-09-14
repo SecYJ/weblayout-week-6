a3f092eb8d9e5cf0d42a0dca81eb37fbdccd2fc3
SecYJ <qianqian1903@gmail.com>
2021-09-14 20:35:45 +0800

    remove empty div, move swiper instance to all.js, remove typography folder and move bg.scss to helper folder

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,25 @@
-console.log('Hello!');
+const swiper = new Swiper(".swiper", {
+	loop: true,
+	slidesPerView: 1,
+	spaceBetween: 30,
+	grid: {
+		rows: 1,
+		fill: "row",
+	},
+	autoplay: {
+		delay: 2500,
+		disableOnInteraction: false,
+		pauseOnMouseEnter: true,
+	},
 
-$(document).ready(() => {
-  console.log('HesSchool Hello!');
+	breakpoints: {
+		576: {
+			slidesPerView: 2,
+			spaceBetween: 30,
+		},
+		768: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+		},
+	},
 });
